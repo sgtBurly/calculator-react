a338b197ba0887edfcc659c00f1a59d347475718
Johan Markström <johan.markstrom@gmail.com>
2021-02-16 13:41:17 +0100

    added calculator component

---
+++ src/components/Calculator.js
@@ -0,0 +1,10 @@
+import { calculator } from "../css/Calculator.module.css"
+const Calculator = () => {
+    return ( 
+        <div className={calculator}>
+
+        </div>
+     );
+}
+ 
+export default Calculator;
